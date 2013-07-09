383e055bd1adc641c6882826927660082968abf5
Manuel Chamber <angelpkers@hotmail.com>
2013-07-09 20:48:33 +0000

    Forth modification

--- web.js
+++ web.js
@@ -7,11 +7,20 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFile(infile, function (err, data) {
-    if (err) throw err;
-    response.send(data + "read from" + infile);
-  })
+    if (err) {
+       throw err;
+       console.log(err);
+    }
+    response.send(data + " read from " + infile);
+  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
